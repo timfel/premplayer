a0200eb29c4bf5eb78bd25e68864485f46c3a3f2
Tim Felgentreff <timfelgentreff@gmail.com>
2011-02-06 14:45:16 +0100

    folder navigation works

--- app/assistants/first-assistant.js
+++ app/assistants/first-assistant.js
@@ -17,17 +17,54 @@ FirstAssistant.prototype.fillFileList = function(event_or_path, path) {
 			path = "/media/internal";
 		}
 	}
+	$('fileListTitleId').update(path);
+
 	var split_and_convert = function(str, img) {
 		return str.split("/").collect(function(el) {
 			return {data: el, icon: img};
 		});
 	};
 
+	// Testing.
+	// $('fileListId').mojo.noticeAddedItems(0, [{data: "item", icon: "file"}, {data: "item2", icon: "folder"}] );
+	$('fileListId').mojo.noticeRemovedItems(0, $('fileListId').mojo.getLength());
 	var files = split_and_convert($('premplayer_plugin').list_files(path), "file");
 	$('fileListId').mojo.noticeAddedItems(0, files);
 	var directories = split_and_convert($('premplayer_plugin').list_directories(path), "folder");
 	$('fileListId').mojo.noticeAddedItems(0, directories);
 };
+
+writeObj = function(obj, message) {
+  if (!message) { message = obj; }
+  var details = "*****************" + "\n" + message + "\n";
+  var fieldContents;
+  for (var field in obj) {
+    fieldContents = obj[field];
+    if (typeof(fieldContents) == "function") {
+      fieldContents = "(function)";
+    }
+    details += "  " + field + ": " + fieldContents + "\n";
+  }
+  console.log(details);
+};
+
+FirstAssistant.prototype.handleListTap = function(event) {
+	if (event.item.icon === "folder") {
+		var current_path = $('fileListTitleId').innerHTML;
+		if (event.item.data === ".") {
+			// Just refresh
+		} else if (event.item.data === "..") {
+			// Move up
+			current_path = current_path.slice(0, current_path.lastIndexOf("/"));
+		} else {
+			current_path = current_path + "/" + event.item.data;
+		}
+		this.fillFileList(current_path);
+	} else if (event.item.icon === "file") {
+		writeObj(event.item, "TODO: Add this to list");
+	}
+};
+
 
 FirstAssistant.prototype.setup = function() {
 	/* this function is for setup tasks that have to happen when the scene is first created */
@@ -49,6 +86,7 @@ FirstAssistant.prototype.setup = function() {
 			disabled: false
 		}
 	);
+	this.controller.listen(this.controller.get("fileListId"), Mojo.Event.listTap, this.handleListTap.bind(this));
 
 	/* update the app info using values from our app */
 	this.controller.get("app-title").update(Mojo.appInfo.title);
